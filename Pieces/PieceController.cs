14e2e42d2f43ab9603be8d4d540114de0139bf96
Maurice König <koenig_maurice@hotmail.com>
2021-07-18 17:36:04 +0200

    Zusätztlichen Layer "PieceMover" eingeführt, um PieceController zu entlasen. Dabei wurden viele Member aus dem Controller in den Mover verlagert. Access Modifiers wurden ebenfalls überarbeitet.

--- Pieces/PieceController.cs
+++ Pieces/PieceController.cs
@@ -5,89 +5,30 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(PieceMover))]
 public sealed class PieceController : MonoBehaviour
 {
-    private Piece piece;
+    internal Piece piece;
     private PieceBehaviour pieceBehaviour;
-    private List<Square> validMoves = new List<Square>();
-    public bool HasPermissionToMove { get { return GameLogic.Instance.CurrentPlayer == piece.ColorProperty; } private set { } }
-
-    private Camera mainCam;
-    [SerializeField] private Vector3 origin;
-    private bool isDragging = false;
-    private object raycastTarget;
-    [SerializeField] private Square raycastTargetSquare;
-    [SerializeField] private Piece raycastTargetPiece;
-
-   
+    private PieceMover Mover;
 
     private void Start () {
 
-        mainCam = Camera.main;
         piece = GetComponent<Piece>();
         pieceBehaviour = GetComponent<PieceBehaviour>();
+        Mover = GetComponent<PieceMover>();
     }
-    private Vector3 GetWorldPosition (Transform t) {
-
-        var desiredPosition = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCam.nearClipPlane));
-        return desiredPosition;
-    }
-    private void DisableDragging() {
-
-        isDragging = false;
-    }
-    private void SendBackToOrigin() {
-
-        this.transform.position = origin;
-    }
-
-
-
     private void OnMouseDown() {
-        
-        if (!isDragging && HasPermissionToMove) {
 
-            isDragging = true;
-            origin = transform.position;
-            Debug.Log("OnMouseDown");
-
-            foreach (var element in Board.Instance.Pieces.Where(x => x != piece)) {
-
-                element.gameObject.layer = 2;
-            }
-        }
+        Mover.PrepareFireRay();
     }
-
     private void OnMouseDrag() {
 
-        if (isDragging && HasPermissionToMove) {
-
-            transform.position = GetWorldPosition(transform);
-
-            if (Physics.Raycast(transform.position, Vector3.forward, out RaycastHit hit)) {
-
-                raycastTarget = hit.collider.gameObject;
-                Debug.Log(hit.collider.name);
-            }
-
-            Debug.DrawRay(transform.position, Vector3.forward * 100);
-        }
-
-        Debug.Log("OnMouseDrag");
+        Mover.FireRay();
     }
-
     private void OnMouseUp() {
 
-        while (isDragging) {
-
-            DisableDragging();
-            SendBackToOrigin();
-
-            foreach (var element in Board.Instance.Pieces.Where(x => x != piece)) {
-
-                element.gameObject.layer = 10;
-            }
-        }
+        Mover.ResolveFireRay();
     }
 
 }
---
+++ Pieces/PieceMover.cs
@@ -0,0 +1,88 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public sealed class PieceMover : MonoBehaviour
+{
+
+    private Square raycastTarget;
+    private PieceController controller;
+    private Piece piece;
+    private Camera mainCam;
+
+    private List<Square> validMoves = new List<Square>();
+    public bool HasPermissionToMove { get { return GameLogic.Instance.CurrentPlayer == piece.ColorProperty; } private set { } }
+
+    private Vector3 origin;
+    [SerializeField] private bool isDragging = false;
+
+    private void Start () {
+
+        mainCam = Camera.main;
+        controller = GetComponent<PieceController>();
+        piece = GetComponent<Piece>();
+    }
+
+    internal void PrepareFireRay() {
+
+        if (!isDragging && HasPermissionToMove) {
+
+            isDragging = true;
+            origin = transform.position;
+            EnableIgnoreRaycastLayerToAllPieces();
+            Debug.Log("OnMouseDown");
+        }
+    }
+    internal void FireRay() {
+
+        if (isDragging && HasPermissionToMove) {
+
+            transform.position = GetWorldPosition(transform);
+            Debug.DrawRay(transform.position, Vector3.forward * 100);
+
+            if (Physics.Raycast(transform.position, Vector3.forward, out RaycastHit hit)) {
+
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+    internal void ResolveFireRay() {
+
+        while (isDragging) {
+
+            DisableDragging();
+            SendBackToOrigin();
+            DisableIgnoreRaycastLayerToAllPieces();
+        }
+    }
+
+    private void EnableIgnoreRaycastLayerToAllPieces() {
+
+        foreach (var element in Board.Instance.Pieces.Where(x => x != piece)) {
+
+            element.gameObject.layer = 2;
+        }
+    }
+    private void DisableIgnoreRaycastLayerToAllPieces() {
+
+        foreach (var element in Board.Instance.Pieces.Where(x => x != piece)) {
+
+            element.gameObject.layer = 10;
+        }
+    }
+    private Vector3 GetWorldPosition(Transform t) {
+
+        var desiredPosition = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCam.nearClipPlane));
+        return desiredPosition;
+    }
+    private void DisableDragging() {
+
+        isDragging = false;
+    }
+    private void SendBackToOrigin() {
+
+        this.transform.position = origin;
+    }
+}
