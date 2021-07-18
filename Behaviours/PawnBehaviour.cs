b38026f2bb7961d736456ddbf8844dc85ae53252
Maurice König <koenig_maurice@hotmail.com>
2021-07-18 23:27:59 +0200

    Schlag-Verhalten vom Bauern implementiert.

--- Behaviours/PawnBehaviour.cs
+++ Behaviours/PawnBehaviour.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 public class PawnBehaviour : PieceBehaviour
 {
@@ -14,6 +15,7 @@ public class PawnBehaviour : PieceBehaviour
         var temp = this.piece.Coordinates;
         var pawnRef = this.piece as Pawn;
 
+        // ein oder zwei Felder zu Beginn - je nachdem.
         for (int i = 0; i < 2; i++) {
 
             if (this.piece.ColorProperty == ColorField.White) {
@@ -57,6 +59,89 @@ public class PawnBehaviour : PieceBehaviour
                     if (pawnRef.hasNotMovedYet) continue;
                     else break;
 
+                }
+            }
+        }
+
+        temp = this.piece.Coordinates;
+        // Checke für Gegner: OBEN LINKS.
+        for (int i = 0; i < 1; i++) {
+
+            if (this.piece.ColorProperty == ColorField.White) {
+
+                temp.y++; temp.x--;
+                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp).ToList();
+                if (next.Count() == 0) break;
+                var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+
+                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                    if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+
+                        Debug.Log("Oben rechts wurde gecallt!");
+                        validSquares.Add(newSquare);
+                        break;
+                    }
+                }
+            }
+
+            if (this.piece.ColorProperty == ColorField.Black) {
+
+                temp.y--; temp.x++;
+                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp).ToList();
+                if (next.Count() == 0) break;
+                var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+
+                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                    if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+
+                        validSquares.Add(newSquare);
+                        break;
+                    }
+                }
+            }
+        }
+
+        temp = this.piece.Coordinates;
+        // Gegner ermitteln: OBEN RECHTS.
+        for (int i = 0; i < 1; i++) {
+
+            if (this.piece.ColorProperty == ColorField.White) {
+
+                temp.y++; temp.x++;
+                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp).ToList();
+                if (next.Count() == 0) break;
+                var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+
+                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                    if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+
+                        validSquares.Add(newSquare);
+                        break;
+                    }
+                }
+            }
+
+            if (this.piece.ColorProperty == ColorField.Black) {
+
+                temp.y--; temp.x--;
+                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp).ToList();
+                if (next.Count() == 0) break;
+                var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+
+                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                    if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+
+                        validSquares.Add(newSquare);
+                        break;
+                    }
                 }
             }
         }
