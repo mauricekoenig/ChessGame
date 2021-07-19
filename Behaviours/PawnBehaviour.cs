8c8bbef9ff0976623f5b39aa1d8786c1fef9b5b4
Maurice König <koenig_maurice@hotmail.com>
2021-07-20 00:07:55 +0200

    En Passant wurde korrekt implementiert. Zudem wurde in Zeile 222 die Angabe == TurnCounter auf TurnCounter - 1 gesetzt, da der interne Counter einer Figur gesetzt wird, bevor der allgemeine Counter sich erhöht und dieser boolsche Ausdruck somit niemals wahr sein kann. Außderm wurde der Code lesbarer gemacht... bedingt durch Linieneinrückungen.

--- Behaviours/PawnBehaviour.cs
+++ Behaviours/PawnBehaviour.cs
@@ -19,45 +19,38 @@ public class PawnBehaviour : PieceBehaviour
         for (int i = 0; i < 2; i++) {
 
             if (this.piece.ColorProperty == ColorField.White) {
-
-
                 temp.y++;
-                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp).ToList();
-                if (next.Count() == 0) break;
-                var newSquare = next.Single();
-
-                if (newSquare.CurrentSubscriber != null) {
-
-                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
-                    if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) break;
+                    var next = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                        if (next.Count() == 0) break;
+                            var newSquare = next.Single();
+                                if (newSquare.CurrentSubscriber != null) {
+                                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                                        if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) break;
                 }
 
                 else if (newSquare.CurrentSubscriber == null) {
-
                     validSquares.Add(newSquare);
-                    if (pawnRef.hasNotMovedYet) continue;
-                    else break;
+                        if (pawnRef.hasNotMovedYet) continue; // wenn bereits bewegt, wird dieser Vorgang nicht mehr wiederholt. Ansonsten wird ein weiteres Feld gescannt.
+                            else break;
                 }
             }
 
+            // -----------------------------------------------------------------------------------------------------------------------------------
+
             else if (this.piece.ColorProperty == ColorField.Black) {
-
                 temp.y--;
-                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp).ToList();
-                if (next.Count() == 0) break;
-                var newSquare = next.Single();
-
-                if (newSquare.CurrentSubscriber != null) {
-
-                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
-                    if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) break;
+                    var next = Board.Instance.Squares.Where(x => x.Coordinates == temp).ToList();
+                        if (next.Count() == 0) break;
+                            var newSquare = next.Single();
+                                if (newSquare.CurrentSubscriber != null) {
+                                    if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                                        if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) break;
                 }
 
                 else if (newSquare.CurrentSubscriber == null) {
-
                     validSquares.Add(newSquare);
-                    if (pawnRef.hasNotMovedYet) continue;
-                    else break;
+                        if (pawnRef.hasNotMovedYet) continue;
+                            else break;
 
                 }
             }
@@ -145,26 +138,158 @@ public class PawnBehaviour : PieceBehaviour
             }
         }
 
+        // En Passant Links
+        temp = this.piece.Coordinates;
+        for (int i = 0; i < 1; i++) {
+
+            if (this.piece.ColorProperty == ColorField.White) {
+
+                temp.x--;
+                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                if (next.Count() == 0) break;
+                var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+                    if (newSquare.CurrentSubscriber.GetType() == typeof(Pawn)) {
+                        if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                            if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+                                Debug.Log("Schwarzer Bauer, der En Passant geschlagen werden kann entdeckt.");
+                                    Pawn enemyPawn = newSquare.CurrentSubscriber as Pawn;
+                                        if (ThisPawnIsOnFifthRank() &&
+                                            enemyPawn.InternalCounter == GameLogic.Instance.TurnCounter - 1 &&
+                                                enemyPawn.hasMoved2Fields || enemyPawn.canBeCapturedEnPassant) {
+                                                    enemyPawn.canBeCapturedEnPassant = true;
+                                                         Debug.Log("Dieser weiße Bauer hat alle En Passant Guards überwunden.");
+                                                            temp.y++;
+                                                                var enPassantSquare = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                                                                    if (enPassantSquare.Count() == 0) break;
+                                                                        validSquares.Add(enPassantSquare.Single());
+                            }
+                        }
+                    }
+                }
+            } 
+            
+            else if (this.piece.ColorProperty == ColorField.Black) {
+
+                temp.x--;
+                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                if (next.Count() == 0) break;
+                var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+                    if (newSquare.CurrentSubscriber.GetType() == typeof(Pawn)) {
+                        if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                            if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+                                Debug.Log("Weißer Bauer, der En Passant geschlagen werden kann entdeckt.");
+                                    Pawn enemyPawn = newSquare.CurrentSubscriber as Pawn;
+                                        if (ThisPawnIsOnFifthRank() &&
+                                            enemyPawn.InternalCounter == GameLogic.Instance.TurnCounter - 1 &&
+                                                enemyPawn.hasMoved2Fields || enemyPawn.canBeCapturedEnPassant) {
+                                                    enemyPawn.canBeCapturedEnPassant = true;
+                                                        Debug.Log("Dieser schwarze Bauer hat alle En Passant Guards überwunden.");
+                                                            temp.y--;
+                                                                var enPassantSquare = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                                                                    if (enPassantSquare.Count() == 0) break;
+                                                                        validSquares.Add(enPassantSquare.Single());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        temp = this.piece.Coordinates;
+        // En Passant RECHTS.
+        for (int i = 0; i < 1; i++) {
+
+            if (this.piece.ColorProperty == ColorField.White) {
+
+                temp.x++;
+                    var next = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                        if (next.Count() == 0) break;
+                            var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+                    if (newSquare.CurrentSubscriber.GetType() == typeof(Pawn)) {
+                        if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                            if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+                                Debug.Log("Schwarzer Bauer, der En Passant geschlagen werden kann entdeckt.");
+                                    Pawn enemyPawn = newSquare.CurrentSubscriber as Pawn;
+                                        if (ThisPawnIsOnFifthRank() &&
+                                            enemyPawn.InternalCounter == GameLogic.Instance.TurnCounter - 1 &&
+                                                enemyPawn.hasMoved2Fields || enemyPawn.canBeCapturedEnPassant) {
+                                                    enemyPawn.canBeCapturedEnPassant = true;
+                                                     Debug.Log("Dieser weiße Bauer hat alle En Passant Guards überwunden.");
+                                                        temp.y++;
+                                                            var enPassantSquare = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                                                                if (enPassantSquare.Count() == 0) break;
+                                                                    validSquares.Add(enPassantSquare.Single());
+                            }
+                        }
+                    }
+                }
+            }
+
+            else if (this.piece.ColorProperty == ColorField.Black) {
+
+                temp.x++;
+                var next = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                if (next.Count() == 0) break;
+                var newSquare = next.Single();
+
+                if (newSquare.CurrentSubscriber != null) {
+                    if (newSquare.CurrentSubscriber.GetType() == typeof(Pawn)) {
+                        if (newSquare.CurrentSubscriber.ColorProperty == this.piece.ColorProperty) break;
+                            if (newSquare.CurrentSubscriber.ColorProperty != this.piece.ColorProperty) {
+                                Debug.Log("Weißer Bauer, der En Passant geschlagen werden kann entdeckt.");
+                                    Pawn enemyPawn = newSquare.CurrentSubscriber as Pawn;
+                                        if (ThisPawnIsOnFifthRank() &&
+                                             enemyPawn.InternalCounter == GameLogic.Instance.TurnCounter - 1 &&
+                                                enemyPawn.hasMoved2Fields || enemyPawn.canBeCapturedEnPassant) {
+                                                    enemyPawn.canBeCapturedEnPassant = true;
+                                                        Debug.Log("Dieser schwarze Bauer hat alle En Passant Guards überwunden.");
+                                                             temp.y--;
+                                                                 var enPassantSquare = Board.Instance.Squares.Where(x => x.Coordinates == temp);
+                                                                    if (enPassantSquare.Count() == 0) break;
+                                                                        validSquares.Add(enPassantSquare.Single());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         return validSquares;
     }
+
     protected override void Start() {
 
         base.Start();
     }
-    private bool PawnIsOnFifthRank () {
+    private bool ThisPawnIsOnFifthRank () {
 
         if (this.piece.ColorProperty == ColorField.Black) {
+            if (this.piece.Coordinates.y == 4) {
+                Debug.Log("Schwarzer Bauer ist auf dem fünften Rang.");
+                    return true;
+            }
 
-            if (this.piece.Coordinates.y == 4) return true;
-            else return false;
+            else 
+                return false;
         } 
         
         else if (this.piece.ColorProperty == ColorField.White) {
+            if (this.piece.Coordinates.y == 5) {
+                 Debug.Log("Weißer Bauer ist auf dem fünften Rang.");
+                    return true;
+            }
 
-            if (this.piece.Coordinates.y == 5) return true;
-            else return false;
+            else 
+                return false;
         }
         
-        else return false;
+        else 
+            return false;
     }
 }
