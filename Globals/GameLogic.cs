ee0ca6d2d7afef7929d522d69b82cab0cb4cd5fd
Maurice König <koenig_maurice@hotmail.com>
2021-07-20 00:08:44 +0200

    Erste Implementierung vom Zugwechsel-Verhalten. TurnCounter wird nun automatisch innerhalb der Funktion erhöht. Im Vorfeld wurde dies noch über eine seperate Funktion erledigt.

--- Globals/GameLogic.cs
+++ Globals/GameLogic.cs
@@ -9,11 +9,28 @@ public class GameLogic : MonoBehaviour
 {
     public static GameLogic Instance;
     public ColorField CurrentPlayer { get; private set; } = ColorField.White;
-    public int TurnCounter { get; set; } = 0;
+    public int TurnCounter { get; private set; } = 0;
+    public int Turn_Counter;
 
     private void Awake () {
 
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
+    }
+    protected void Update() {
+        Turn_Counter = TurnCounter;
+    }
+
+    public void ChangeActivePlayer() {
+
+        if (CurrentPlayer == ColorField.Black) {
+                CurrentPlayer = ColorField.White;
+                    TurnCounter++;
+        } 
+
+        else if (CurrentPlayer == ColorField.White) {
+            CurrentPlayer = ColorField.Black;
+                TurnCounter++;
+        }
     }
 }
