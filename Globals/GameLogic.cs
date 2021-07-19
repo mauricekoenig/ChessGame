22e1032f98aa8ca49135737549fc181871f4ab8e
Maurice König <koenig_maurice@hotmail.com>
2021-07-19 18:44:00 +0200

    TurnCounter eingeführt, um bei Bewegung von Figuren den Zeitpunkt der Bewegung festhalten zu können und um eine allgemeine Dokumentation der bisherigen Züge zu haben. Außerdem wurden die alten Events entfernt!

--- Globals/GameLogic.cs
+++ Globals/GameLogic.cs
@@ -9,22 +9,11 @@ public class GameLogic : MonoBehaviour
 {
     public static GameLogic Instance;
     public ColorField CurrentPlayer { get; private set; } = ColorField.White;
-    public event Action OnPlayerPlayedTurn;
+    public int TurnCounter { get; set; } = 0;
 
     private void Awake () {
 
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
-
-        OnPlayerPlayedTurn += ChangeCurrentPlayer;
-    }
-    public void InvokeOnPlayerPlayedTurn() {
-
-        OnPlayerPlayedTurn.Invoke();
-    }
-    public void ChangeCurrentPlayer () {
-
-        if (CurrentPlayer == ColorField.Black) CurrentPlayer = ColorField.White;
-        else if (CurrentPlayer == ColorField.White) CurrentPlayer = ColorField.Black;
     }
 }
