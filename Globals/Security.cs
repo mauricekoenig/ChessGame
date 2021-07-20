f7811fc86aaf5eb870c32ad62636eb221f3b39bf
Maurice König <koenig_maurice@hotmail.com>
2021-07-21 01:12:43 +0200

    "Security" Klasse eingefügt, um globale Kontrolle im Spiel zu managen.

---
+++ Globals/Security.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+public static class Security
+{
+    public static bool GlobalPermission { get; private set; } = true;
+    public static void Lock () {
+
+        if (GlobalPermission) GlobalPermission = false;
+        else return;
+    }
+    public static void Unlock () {
+
+        if (!GlobalPermission) GlobalPermission = true;
+        else return;
+    }
+}
