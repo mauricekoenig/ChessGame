b5f220c5faebfc823612bfa19014f705e81bf421
Maurice König <koenig_maurice@hotmail.com>
2021-07-22 00:12:03 +0200

    Neues Attribut eingeführt um temporären Speicher zu kennzeichnen.

---
+++ Attributes/CachePropertyAttribute.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+
+namespace MauriceKoenig.ChessGame
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class CachePropertyAttribute : Attribute
+    {
+        public string Comment { get; }
+        public CachePropertyAttribute() {
+
+        }
+        public CachePropertyAttribute (string comment) {
+            this.Comment = comment;
+        }
+    }
+}
