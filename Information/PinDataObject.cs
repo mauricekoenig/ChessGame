fb731615b3ee34c9e2776e5bb711fd205d6ca74d
Maurice König <koenig_maurice@hotmail.com>
2021-07-23 18:30:39 +0200

    Neue Klasse um Pin-Data zu encapsulaten.

---
+++ Information/PinDataObject.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+namespace MauriceKoenig.ChessGame
+{
+    public sealed class PinDataObject
+    {
+        public bool IsPinned { get; set; }
+        public bool CanCaptureAttacker { get; set; }
+        public BasePiece PinnedBy { get; set; }
+        public List<Square> OptionalMoves;
+
+        public PinDataObject (bool isPinned, bool canCapture, BasePiece pinnedBy, List<Square> optionalMoves) {
+
+        }
+
+    }
+}
